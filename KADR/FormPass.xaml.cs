769cfa54c6ac0621d6055817c9fe828073484f67
Admin <lglg22888@gmail.com>
2025-04-03 23:54:13 +0300

    Autorization form

---
+++ KADR/FormPass.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KADR
+{
+    /// <summary>
+    /// Логика взаимодействия для FormPass.xaml
+    /// </summary>
+    public partial class FormPass : Window
+    {
+        public FormPass()
+        {
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
